f261b6b5bdbf787aa4c16885a78ef6ca71655c5d
Eduardo Galbiati <edugalb@boxbasecapital.com>
2016-10-27 22:38:38 -0300

    Adicionado comentarios

--- web/index.php
+++ web/index.php
@@ -11,11 +11,14 @@ require_once __DIR__.'/../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+// Cria instância de Silex\Application e armazena em $app
 require_once dirname(__DIR__).'/src/app.php';
 
+// Define a rota
 $app->get( 'api/v1/vagas', function (Request $request) use ($app) {
     $ctrl = new App\Vagas\Controller\VagasController($app);
     return $ctrl->listVagas($request);
 });
 
+// Executa o Silex\Application
 $app->run();
